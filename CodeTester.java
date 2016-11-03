9c3371908abf468d3b023fb12a774cca7dd38645
Ofek Gila <ofek@theofekfoundation.org>
2016-11-02 17:11:41 -0700

    Reorder imports

--- CodeTester.java
+++ CodeTester.java
@@ -1,15 +1,15 @@
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.InvocationTargetException;
-
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * CodeTester.java
